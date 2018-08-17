971b7623789952c495f558e91f6a93063fefb57b
TuanJio <tuannx@ominext.com>
2018-08-17 17:50:30 +0700

    Up up

--- app/src/main/java/jio/customview/app/presentation/activity/MainActivity.kt
+++ app/src/main/java/jio/customview/app/presentation/activity/MainActivity.kt
@@ -3,11 +3,14 @@ package jio.customview.app.presentation.activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import jio.customview.app.R
+import rx.Observable
+import rx.Subscriber
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +22,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
         btnTestRecycleView.text = "sdads"
+
+        exampleRxJava()
     }
 
     @OnClick(R.id.btn_test_recycle_view)
     fun openExampleRecycleView() {
         startActivity(Intent(this, JioRecycleViewActivity::class.java))
     }
+
+    private fun exampleRxJava() {
+
+    }
+
+    fun getDetail(): Int {
+        return 100
+    }
+
+    var observable = Observable
+            .from(arrayOf(1, 2, 3))
+            .subscribe(object : Subscriber<Int>() {
+                override fun onNext(number: Int) {
+                    Log.e("=====>onNext", number.toString())
+                }
+
+                override fun onCompleted() {
+                    Log.e("==>observable:", "Complete")
+                }
+
+                override fun onError(e: Throwable) {
+                    Log.e("==>observable:", "onError")
+                }
+            })
+
+
+    data class Student(var name: String, var tuoi: Int)
 }
